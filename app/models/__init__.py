94908b6f85c87b6f7729dc9d2ad9a6aa570246a7
aseef <contact@aseef.dev>
2025-03-13 21:52:21 -0400

    fixed a circular import issue

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,9 @@
+from .uploaded_file import UploadedFile
 from .user import User, PersonalAuthenticationToken
 from .token import AccessToken
 from .course import Course
 from .assignment import Assignment, Question
 from .course_material import CourseMaterial
-from .uploaded_file import UploadedFile
 from .student import Student
 from .student_response import StudentResponse, ResponseData
 from .grade import Grade
