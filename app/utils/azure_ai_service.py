719f2435a04fe8b60f093fc6afb4b375bb5e365c
aseef <contact@aseef.dev>
2025-03-23 22:20:01 -0400

    Now using service principals + improved logging

--- app/utils/azure_ai_service.py
+++ app/utils/azure_ai_service.py
@@ -1,17 +1,18 @@
 from typing import Optional
-from azure.ai.ml import MLClient
+#from azure.ai.ml import MLClient
 
 azure_ai_service: Optional["AzureAIService"] = None
 
 
 class AzureAIService:
-    ...  # TODO
+    def __init__(self, credential):
+        ...  # TODO
 
     @staticmethod
-    def init_singleton(*TODO):
+    def init_singleton(credential):
         # TODO
         global azure_ai_service
-        azure_ai_service = AzureAIService()
+        azure_ai_service = AzureAIService(credential)
 
     @staticmethod
     def get_instance() -> Optional["AzureAIService"]:
---
+++ app/utils/env_var_util.py
@@ -0,0 +1,52 @@
+import os
+
+
+def get_str_var(var_name, default=None) -> str:
+    """
+    Get a string environment variable value or return a default value if not found.
+    If no default value is specified or the default value is None this method
+    will raise an exception.
+    :param var_name: Name of the environment variable
+    :param default: Default value to return if the environment variable is not found
+    :return: The environment variable value or the default value
+    """
+    ret = os.getenv(var_name, default)
+    if ret is None:
+        raise Exception("Environment variable '{}' not found.".format(var_name))
+    return ret
+
+
+def get_int_var(var_name, default=None) -> int:
+    """
+    Get an integer environment variable value or return a default value if not found
+    If no default value is specified or the default value is None this method
+    will raise an exception.
+    :param var_name: Name of the environment variable
+    :param default: Default value to return if the environment variable is not found
+    :return: The environment variable value or the default value
+    """
+    return int(get_str_var(var_name, default))
+
+
+def get_float_var(var_name, default=None) -> float:
+    """
+    Get a float environment variable value or return a default value if not found
+    If no default value is specified or the default value is None this method
+    will raise an exception.
+    :param var_name: Name of the environment variable
+    :param default: Default value to return if the environment variable is not found
+    :return: The environment variable value or the default value
+    """
+    return float(get_str_var(var_name, default))
+
+
+def get_bool_var(var_name, default=None) -> bool:
+    """
+    Get a boolean environment variable value or return a default value if not found
+    If no default value is specified or the default value is None this method
+    will raise an exception.
+    :param var_name: Name of the environment variable
+    :param default: Default value to return if the environment variable is not found
+    :return: The environment variable value or the default value
+    """
+    return get_str_var(var_name, default).lower() in ['true', '1', 't', 'y', 'yes']
