dab2f3af99714e42e1df3e57212ac2c6384b3828
fahuddin <fahuddin@bu.edu>
2025-04-13 17:24:10 -0400

    added loggin statements

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6836b51677b4d955"; }
+/******/ 		__webpack_require__.h = function() { return "9924c92da2a002f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.6f0aea8ab9b7d7a8.hot-update.js
+++ frontend/.next/static/webpack/webpack.6f0aea8ab9b7d7a8.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c30371935583ba9f"; }
+/******/ 	__webpack_require__.h = function() { return "9924c92da2a002f6"; }
 /******/ }();
 /******/ 
 /******/ }
