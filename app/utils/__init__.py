52ed03f37eff7a76f294e0cc706cdc82dfd5c04e
aseef <contact@aseef.dev>
2025-04-17 18:03:25 -0400

    deleted job manager

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -4,5 +4,4 @@ from .vector_db_service import ChromaDBService, VectorDBService
 from .jwt_service import JWTService, UserToken
 from .env_var_util import get_str_var, get_int_var, get_float_var, get_bool_var
 from .logging_util import setup_loggers
-from .job_manager import BackgroundJobManager
 from .bg_material_processor import BackgroundMaterialProcessor
